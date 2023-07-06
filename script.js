cbe0cf9b74c67d3fa36f8df2591a6c2f37b84f8f
Justin Hobbs <justinhobbs@fastmail.com>
2023-07-06 17:01:14 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('.navbar');
+
+header.classList.add('navbarDark');
+
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
